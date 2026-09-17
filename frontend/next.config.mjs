aef0184625d4356f8b407db223ee1f104ed35279
soroosh <sorooshdpki@gmail.com>
2026-09-17 14:55:41 +0330

    feat(frontend): add security headers to next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY', // Prevents site from being framed (clickjacking protection)
+          },,
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff', // Prevents the browser from guessing the content type
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin', // Controls how much referrer info is passed
+          }
+        ]
+      },
+    ];
+  },
 };
 
 export default nextConfig;
